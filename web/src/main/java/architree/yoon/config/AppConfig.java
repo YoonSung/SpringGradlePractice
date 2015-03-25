bc98d4c7f8a62063c010a5375b0df81a1bd57fe7
Yoonsung <lvev9925@naver.com>
2015-03-25 19:42:32 +0900

    RePackaging, Convert xml to java configuration

---
+++ web/src/main/java/architree/yoon/config/AppConfig.java
@@ -0,0 +1,13 @@
+package architree.yoon.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by yoon on 15. 3. 25..
+ */
+@Configuration
+@ComponentScan({"architree.yoon.web"})
+public class AppConfig {
+
+}
--- web/src/main/java/architree/yoon/web/TestController.java
+++ web/src/main/java/architree/yoon/web/TestController.java
@@ -1,4 +1,4 @@
-package web;
+package architree.yoon.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
