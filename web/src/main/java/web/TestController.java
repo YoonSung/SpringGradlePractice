834748b3ecaab2bd083e818bd14183626b877f24
Yoonsung <lvev9925@naver.com>
2015-03-25 19:08:45 +0900

    Spring Web Container Setting

---
+++ web/src/main/java/web/TestController.java
@@ -0,0 +1,19 @@
+package web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by yoon on 15. 3. 25..
+ */
+@Controller
+public class TestController {
+
+    @RequestMapping("/")
+    public @ResponseBody String test() {
+        System.out.println("test");
+        return "First Test";
+    }
+
+}
